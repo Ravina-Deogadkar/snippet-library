45a205f13e8f6b15e7e6c0603155152851cc1ca0
Ravina-Deogadkar <ravina.deogadkar@gmail.com>
2023-03-16 19:53:48 +0530

    Add Header and Landing page | Ravina

---
+++ src/component/LandingPage/Pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from '../component/Header'
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    )
+  }
+}
